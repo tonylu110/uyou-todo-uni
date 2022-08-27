8e7dcf5a71f02a446accb8a015edda7cc206272f
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2022-08-27 17:10:13 +0800

    Update: support android navigation transparent

--- App.vue
+++ App.vue
@@ -1,6 +1,22 @@
 <script>
 	export default {
 		onLaunch: function() {
+			uni.getSystemInfo({
+				success: (res) => {
+					if (res.osName === 'android') {
+						var Color = plus.android.importClass("android.graphics.Color");
+						plus.android.importClass("android.view.Window");    
+						var mainActivity = plus.android.runtimeMainActivity();    
+						var window_android = mainActivity.getWindow();    
+						var WindowManager=plus.android.importClass("android.view.WindowManager");    
+						var View=plus.android.importClass("android.view.View");    
+						window_android.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);  
+						window_android.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN  | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);  
+						window_android.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);   
+						window_android.setNavigationBarColor(Color.TRANSPARENT);  
+					}
+				}
+			})
 			console.log('App Launch')
 		},
 		onShow: function() {
